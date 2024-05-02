f70317ffff2f18a004dd7d55dddc2622f5680eb8
ShellTooSleepy <sjgallegos270@gmail.com>
2024-05-02 14:51:13 -0700

    Home page UI updated - added form for new queries but havent connected it to backend

--- phase_3_queries.sql
+++ phase_3_queries.sql
@@ -1,9 +1,11 @@
 -- 1. most expensive item in each category
+-- NO INPUT NEEDED
 select title, category, price
 from item
 where price in (select max(price) from item group by category)
 
 -- 2. shell can be changed to any user
+-- **INPUT NEEDED**
 select i.title, i.descr, i.category, i.username as Poster, i.category, r.username as Reviewer, r.text,r.rating
 from item as i, reviews as r
 where i.username = "shell" and i.itemID = r.itemID and (r.rating = "excellent" or r.rating = "good")
@@ -12,6 +14,7 @@ where i.username = "shell" and i.itemID = r.itemID and (r.rating = "excellent" o
 --    for what has the highest freq or display all with 
 --    highest freq if there are duplicates
 --    postDate can be changed to any date
+-- CHOOSE WHETHER TO HARD CODE OR NOT
 select username, count(username) as freq
 from item 
 where postDate = "2024-04-29"
@@ -21,6 +24,7 @@ order by freq desc
 -- 4. WTF is favorited mean???
 
 -- 5. Display all users that dont have 3 or more excellent reviews for a any item
+-- NO INPUT NEEDED
 select username 
 from user
 where username not in (
@@ -45,6 +49,7 @@ where username not in (
 -- )
 
 -- 6. all users that have given only reviews with a poor rating
+-- NO INPUT NEEDED
 select username
 from reviews as r
 group by username
